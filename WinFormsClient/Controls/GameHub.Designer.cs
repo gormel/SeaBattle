08ed4183bae20cda8efdd2ddb0eaf94238f8af83
Roman <gremkil@gmail.com>
2016-03-13 17:50:56 +0300

    ***

--- WinFormsClient/Controls/GameHub.Designer.cs
+++ WinFormsClient/Controls/GameHub.Designer.cs
@@ -30,14 +30,19 @@
 		{
 			this.mRoomsListBox = new System.Windows.Forms.ListBox();
 			this.mRefreshButton = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.mRoomName = new System.Windows.Forms.TextBox();
+			this.mJoinButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// mRoomsListBox
 			// 
+			this.mRoomsListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
 			this.mRoomsListBox.FormattingEnabled = true;
 			this.mRoomsListBox.Location = new System.Drawing.Point(3, 32);
 			this.mRoomsListBox.Name = "mRoomsListBox";
-			this.mRoomsListBox.Size = new System.Drawing.Size(221, 303);
+			this.mRoomsListBox.Size = new System.Drawing.Size(221, 433);
 			this.mRoomsListBox.TabIndex = 0;
 			// 
 			// mRefreshButton
@@ -50,16 +55,49 @@
 			this.mRefreshButton.UseVisualStyleBackColor = true;
 			this.mRefreshButton.Click += new System.EventHandler(this.mRefreshButton_Click);
 			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Location = new System.Drawing.Point(149, 469);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Create";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// mRoomName
+			// 
+			this.mRoomName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.mRoomName.Location = new System.Drawing.Point(3, 471);
+			this.mRoomName.Name = "mRoomName";
+			this.mRoomName.Size = new System.Drawing.Size(140, 20);
+			this.mRoomName.TabIndex = 3;
+			// 
+			// mJoinButton
+			// 
+			this.mJoinButton.Location = new System.Drawing.Point(3, 3);
+			this.mJoinButton.Name = "mJoinButton";
+			this.mJoinButton.Size = new System.Drawing.Size(75, 23);
+			this.mJoinButton.TabIndex = 4;
+			this.mJoinButton.Text = "Join";
+			this.mJoinButton.UseVisualStyleBackColor = true;
+			this.mJoinButton.Click += new System.EventHandler(this.mJoinButton_Click);
+			// 
 			// GameHub
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.mJoinButton);
+			this.Controls.Add(this.mRoomName);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.mRefreshButton);
 			this.Controls.Add(this.mRoomsListBox);
 			this.Name = "GameHub";
-			this.Size = new System.Drawing.Size(719, 514);
+			this.Size = new System.Drawing.Size(733, 494);
 			this.Load += new System.EventHandler(this.GameHub_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -67,5 +105,8 @@
 
 		private System.Windows.Forms.ListBox mRoomsListBox;
 		private System.Windows.Forms.Button mRefreshButton;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox mRoomName;
+		private System.Windows.Forms.Button mJoinButton;
 	}
 }
---
+++ WinFormsClient/Controls/GameHub.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Core.Packeges;
+
+namespace WinFormsClient
+{
+	public partial class GameHub : UserControl
+	{
+		private class ListBoxItem
+		{
+			public Guid ID { get; set; }
+			public string Name { get; set; }
+			public int UserCount { get; set; }
+
+			public override string ToString()
+			{
+				return string.Format("{0} ({1})", Name, UserCount);
+			}
+		}
+		private readonly MainForm mMainForm;
+
+		public GameHub(MainForm mainForm)
+		{
+			this.mMainForm = mainForm;
+			InitializeComponent();
+		}
+
+		private async void GameHub_Load(object sender, EventArgs e)
+		{
+			mMainForm.Resolver.AddHandler<RoomList>(RoomListRecived);
+			mMainForm.Resolver.AddHandler<RoomInfoPackage>(RoomInfoRecived);
+			mMainForm.Resolver.AddHandler<RoomCreatePackage>(RoomCreateRecived);
+			mMainForm.Resolver.AddHandler<JoinRoom>(JoinRoomRecived);
+			RefreshRooms();
+		}
+
+		private void JoinRoomRecived(JoinRoom joinRoom)
+		{
+			if (!joinRoom.Result)
+				return;
+		}
+
+		private void RoomCreateRecived(RoomCreatePackage roomCreatePackage)
+		{
+			RefreshRooms();
+		}
+
+		private async void RoomListRecived(RoomList list)
+		{
+			mRoomsListBox.Items.Clear();
+			foreach (var room in list.Rooms)
+			{
+				 await mMainForm.Talker.Send(mMainForm.Me, new RoomInfoPackage
+				{
+					ID = room
+				});
+			}
+		}
+
+		private async void RefreshRooms()
+		{
+			 await mMainForm.Talker.Send(mMainForm.Me, new RoomList());
+		}
+
+		private void RoomInfoRecived(RoomInfoPackage info)
+		{
+			mRoomsListBox.Items.Add(new ListBoxItem { ID = info.ID, Name = info.Name, UserCount = info.UserCount });
+		}
+
+		private void mRefreshButton_Click(object sender, EventArgs e)
+		{
+			RefreshRooms();
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			 await mMainForm.Talker.Send(mMainForm.Me, new RoomCreatePackage {Name = mRoomName.Text});
+		}
+
+		private async void mJoinButton_Click(object sender, EventArgs e)
+		{
+			await mMainForm.Talker.Send(mMainForm.Me, new JoinRoom { ID = ((ListBoxItem)mRoomsListBox.SelectedItem).ID });
+		}
+	}
+}
